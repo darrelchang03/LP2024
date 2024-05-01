881c75f8eb6e251d75eaea64631941aeebf1ab21
darrelchang03 <darrelchang@gmail.com>
2024-04-30 23:25:55 -0700

    Laptop mongoose connection

--- server.js
+++ server.js
@@ -23,13 +23,37 @@ app.use(expressLayouts)
 
 
 // Connect to MongoDB
-const mongoose = require('mongoose')
-const { dir } = require('console')
-mongoose.connect(process.env.DATABASE_URL, { 
-     })
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to mongoose'))
+// const mongoose = require('mongoose')
+// const { dir } = require('console')
+// mongoose.connect(process.env.DATABASE_URL, { 
+//      })
+// const db = mongoose.connection
+// db.on('error', error => console.error(error))
+// db.once('open', () => console.log('Connected to mongoose'))
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://dbUser:Black10280821!cedarrocket@learningproject.lpw241q.mongodb.net/?retryWrites=true&w=majority&appName=LearningProject";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
